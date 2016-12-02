bf8b7aacb2a1e4bc6bbfcd17baa1474981fd5ae0
Daniel Tischner <Slippy-Toad@web.de>
2016-12-02 14:16:21 +0100

    Finished dropdown functionallity
    Added full dynamic dropdown functionallity for the website

--- res/examples/webdemo/search.js
+++ res/examples/webdemo/search.js
@@ -23,6 +23,10 @@ var isMapReady = false;
  * Whether the map object is currently hidden or not.
  */
 var isMapHidden = true;
+/**
+ * Holds current data of computed matches.
+ */
+var matchesData;
 
 /**
  * Callback method which is used by the Google Maps API
@@ -54,6 +58,12 @@ function initMap() {
  */
 function queryRoutine() {
 	var query = $("#query").val();
+	
+	// Clear dropdown if query is empty
+	if (!query.trim().length) {
+		$('.suggestionBoxWrapper').empty();
+	}
+	
     var host = window.location.host;
     var url = "http://" + host + "/?q=" + query;
 	$.ajax(url, {dataType: 'jsonp'});
@@ -89,8 +99,59 @@ function queryServerCallback(json) {
 	var latPos = firstMatch[1];
 	var longPos = firstMatch[2];
 	
-	// TODO Change to drop-down build-call
-	displayPosition(latPos, longPos);
+	// Remember matches
+	matchesData = json.matches;
+	
+	buildDropDown(matchesData);
+}
+
+/**
+ * Builds the drop down menu for the given matches.
+ */
+function buildDropDown(matches) {
+	var wrapper = $('.suggestionBoxWrapper');
+	// Append a box, if not already contained
+	if (!$('.suggestionBox').length) {
+		$(wrapper).append('<div class="suggestionBox"></div>');
+	} else {
+		// Clear the box from previous results
+		$('.suggestionBox').empty();
+	}
+	
+	// Build the dropdown
+	var box = $('.suggestionBox');
+	var textToAppend = '<ul class="suggestionList">';
+	for (var i = 0; i < matches.length; i++) {
+		var match = matches[i];
+		textToAppend += '<li class="suggestion" onclick="decidedForSuggestion(\'' + match[0] + '\')">';
+		textToAppend += match[0];
+		textToAppend += '</li>';
+	}
+	textToAppend += '</ul>';
+	$(box).append(textToAppend);
+}
+
+/**
+ * Performs actions that need to be taken once decided for a suggestion,
+ * like displaying the match with the given name on the map.
+ */
+function decidedForSuggestion(matchName) {
+	var matchEntry;
+	for (var i = 0; i < matchesData.length; i++) {
+		if (matchesData[i][0] == matchName) {
+			matchEntry = matchesData[i];
+			break;
+		}
+	}
+	
+	// Remove dropdown
+	$('.suggestionBoxWrapper').empty();
+	
+	// Exchange search entry with match
+	$('#query').val(matchEntry[0]);
+	
+	// Display location in map
+	displayPosition(matchEntry[1], matchEntry[2]);
 }
 
 // Hook the routine to an event
